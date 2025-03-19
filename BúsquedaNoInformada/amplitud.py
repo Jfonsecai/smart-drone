807d9237efaf6963756cbab7218f398a3e6cec5e
Jfonsecai <juandavidfon@gmail.com>
2025-03-18 21:51:33 -0500

    Se factoriza el algoritmo de búsqueda por amplitud para optimizarlo, evitando ciclos

--- BúsquedaNoInformada/amplitud.py
+++ BúsquedaNoInformada/amplitud.py
@@ -6,7 +6,9 @@ def amplitud(mundo, filas, columnas, posPaquetes, posInicial):
 
     nodoInicial = Nodo(posInicial, None, set())
     queue = deque([nodoInicial])
-    
+
+    # Lista para almacenar los estados (posición + paquetes recogidos) visitados
+    estadosVisitados = [(posInicial, frozenset())]
 
     while queue:
         nodo = queue.popleft() # Extrae el primer nodo de la cola
@@ -21,13 +23,18 @@ def amplitud(mundo, filas, columnas, posPaquetes, posInicial):
         for movimiento in movimientosPosibles:
             # Verifica si el movimiento está dentro del mundo
             if 0 <= movimiento[0] < filas and 0 <= movimiento[1] < columnas: 
-                if (mundo[movimiento[0]][movimiento[1]] != 1): # Verifica que no hay pared
-                    #paquetes = nodo.paquetes.copy()
+                if (mundo[movimiento[0]][movimiento[1]] != 1): # Verifica que no es un obstáculo
+                    # Si cumple ambas condiciones, es un movimiento válido
                     paquetes = set(nodo.paquetes) # Crea un nuevo set con los paquetes recogidos del nodo padre
-                    if (movimiento in posPaquetes): # Si la nueva posición es un paquete, se añade la coordenada del paquete recogido
-                        paquetes.add(movimiento)
-                    queue.append(Nodo(movimiento, nodo, paquetes)) # Se agrega el nodo a la cola
-                    #print("Nodo expandido: ", nodo)
+                    if (movimiento in posPaquetes): 
+                        paquetes.add(movimiento) # Si la nueva posición es un paquete, se añade la coordenada del paquete recogido
+
+                    estado = (movimiento, frozenset(paquetes))  # Estado actual, con posición y paquetes recogidos
+
+                    # Verificar si este estado ha sido visitado antes
+                    if estado not in estadosVisitados:
+                        estadosVisitados.append(estado)  # Si no ha sido visitado, se añade a los estados visitados
+                        queue.append(Nodo(movimiento, nodo, paquetes)) # Se agrega el nodo a la cola
         
 
 
